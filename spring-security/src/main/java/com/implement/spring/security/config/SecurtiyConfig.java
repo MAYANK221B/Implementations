2cb8f01898bf50ef96d96c834286aed96e83a621
MAYANK221B <38593084+MAYANK221B@users.noreply.github.com>
2024-04-11 19:44:11 +0530

    Jwt token

--- spring-security/src/main/java/com/implement/spring/security/config/SecurtiyConfig.java
+++ spring-security/src/main/java/com/implement/spring/security/config/SecurtiyConfig.java
@@ -2,11 +2,17 @@ package com.implement.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,8 +20,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurtiyConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
+        http.csrf().disable().authorizeHttpRequests((authz) -> authz.requestMatchers(new
+                                AntPathRequestMatcher("/authenticate",HttpMethod.POST.toString())).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
@@ -25,6 +31,12 @@ public class SecurtiyConfig  {
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
--- spring-security/src/main/java/com/implement/spring/security/controller/SecurityController.java
+++ spring-security/src/main/java/com/implement/spring/security/controller/SecurityController.java
@@ -1,14 +1,38 @@
 package com.implement.spring.security.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.implement.spring.security.entity.AuthRequest;
+import com.implement.spring.security.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SecurityController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @GetMapping("/")
     public String code(){
         return "This is your code";
     }
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception{
+        try{
+            authenticationManager.authenticate(
+                   new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+        }
+        catch (Exception e){
+            throw new Exception("invalid username/password");
+        }
+
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
+
 }
